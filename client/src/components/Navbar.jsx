91ce9c93107e29eb28f69b9362488d0420981606
Abhinav <rk7772534@gmail.com>
2023-04-21 12:11:36 +0530

    updated styling for navbar and add icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouseUser,
+  faPenAlt,
+  faRegistered,
+  faSignIn,
+  faSignInAlt,
+  faSignOut,
+  faUserLock,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const localData = JSON.parse(localStorage.getItem("userInfo"));
@@ -8,24 +18,39 @@ const Navbar = () => {
     <nav className="Navbar">
       <ul className="navbarContainer">
         <Link className="itemLink" to={"/"}>
-          <li className="menuItem">Home</li>
+          <li className="menuItem">
+            <FontAwesomeIcon icon={faHouseUser} />
+            <div className="menuItemText">Home</div>
+          </li>
         </Link>
         <Link className="itemLink" to={localData ? "/write" : "/login"}>
-          <li className="menuItem">Write</li>
+          <li className="menuItem">
+            <FontAwesomeIcon icon={faPenAlt} />
+            <div className="menuItemText">Write</div>
+          </li>
         </Link>
 
         {localData === null ? (
           <>
             <Link className="itemLink" to={"/login"}>
-              <li className="menuItem">Login</li>
+              <li className="menuItem">
+                <FontAwesomeIcon icon={faSignInAlt} />
+                <div className="menuItemText">Signup</div>
+              </li>
             </Link>
             <Link className="itemLink" to={"/register"}>
-              <li className="menuItem">Sign Up</li>
+              <li className="menuItem">
+                <FontAwesomeIcon icon={faUserLock} />
+                <div className="menuItemText">Login</div>
+              </li>
             </Link>
           </>
         ) : (
           <Link className="itemLink" to={"/logout"}>
-            <li className="menuItem">Logout</li>
+            <li className="menuItem">
+              <FontAwesomeIcon icon={faSignOut} />
+              <div className="menuItemText">Logout</div>
+            </li>
           </Link>
         )}
       </ul>
