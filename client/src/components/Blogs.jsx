72f2f320cbd8537134447abcb98c6e14ea0f64b4
Abhinav <rk7772534@gmail.com>
2023-05-07 21:07:23 +0530

    add the loading in all blogs rendering 🧨

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -3,6 +3,7 @@ import Blog from "./Blog";
 import { BASE_URL } from "../helper/ref";
 import axios from "axios";
 import BlogImage from "../images/blog1.jpg";
+import ReactLoading from "react-loading";
 import { useState, useEffect } from "react";
 
 import "../styles/Blogs.css";
@@ -10,10 +11,11 @@ import "../styles/Blogs.css";
 const Blogs = ({ searchTitle, setSearchTitle }) => {
   const [allBlogs, setAllBlogs] = useState([]);
   const [fetchError, setFetchError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // make your API call here...
-    console.log(searchTitle);
+    setLoading(true);
     if (searchTitle.length > 0) {
       axios
         .get(`${BASE_URL}/blog/getsearchblogs`, {
@@ -23,19 +25,22 @@ const Blogs = ({ searchTitle, setSearchTitle }) => {
         })
         .then((response) => {
           setAllBlogs(response.data.reverse());
+          setLoading(false);
         })
         .catch((err) => {
           setFetchError(true);
+          setLoading(false);
         });
     } else {
-      console.log("2");
       axios
         .get(`${BASE_URL}/blog/getblogs`)
         .then((response) => {
           setAllBlogs(response.data.reverse());
+          setLoading(false);
         })
         .catch((err) => {
           setFetchError(true);
+          setLoading(false);
         });
     }
   }, [searchTitle]);
@@ -43,30 +48,42 @@ const Blogs = ({ searchTitle, setSearchTitle }) => {
   return (
     <div className="Blogs">
       <h1 className="mainHeading"> Recent Articles </h1>
-      <div className="allBlogs">
-        {fetchError && allBlogs ? (
-          <div>Please wait, Trying to fetch the blogs...</div>
-        ) : (
-          allBlogs &&
-          allBlogs.map((value, index) => {
-            return (
-              <div key={index}>
-                <div className="blogLine"></div>
-                <Blog
-                  blogImage={BlogImage}
-                  heading={value.blogHeading}
-                  blogTags={value.blogTags}
-                  uploadTime={value.blogSaveTime}
-                  userId={value.userId}
-                  minuteRead={value.minuteRead}
-                  blogPreview={value.blogText}
-                  blogId={value._id}
-                />
-              </div>
-            );
-          })
-        )}
-      </div>
+      {loading ? (
+        <div className="loaderContainer">
+          <ReactLoading
+            type={"spin"}
+            color={"#45aaff"}
+            height={50}
+            width={50}
+          />
+          <div className="loadingText">Getting the blogs...</div>
+        </div>
+      ) : (
+        <div className="allBlogs">
+          {fetchError && allBlogs ? (
+            <div>Please wait, Trying to fetch the blogs...</div>
+          ) : (
+            allBlogs &&
+            allBlogs.map((value, index) => {
+              return (
+                <div key={index}>
+                  <div className="blogLine"></div>
+                  <Blog
+                    blogImage={BlogImage}
+                    heading={value.blogHeading}
+                    blogTags={value.blogTags}
+                    uploadTime={value.blogSaveTime}
+                    userId={value.userId}
+                    minuteRead={value.minuteRead}
+                    blogPreview={value.blogText}
+                    blogId={value._id}
+                  />
+                </div>
+              );
+            })
+          )}
+        </div>
+      )}
     </div>
   );
 };
