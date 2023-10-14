d4e3b88bc7732878297dcf5fbcda2d00d65003ea
Abhinav <rk7772534@gmail.com>
2023-10-14 23:27:43 +0530

    updated the navigation system in login and home

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
 import { BASE_URL } from "../helper/ref.js";
@@ -8,11 +8,12 @@ import "../styles/Login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 
-const Login = () => {
+const Login = ({ isReload, setIsReload }) => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
   const navigate = useNavigate();
   const [showPassword, setshowPassword] = useState(false);
+  const [isLoggedInAlready, setIsLoggedInAlready] = useState(false);
 
   function handleShowhide(e) {
     e.preventDefault();
@@ -42,6 +43,7 @@ const Login = () => {
           );
 
           // navigating to home page
+          setIsReload(!isReload);
           navigate("/");
         } else {
           alert("Email/Password is incorrect");
@@ -53,54 +55,70 @@ const Login = () => {
       });
   }
 
+  useEffect(() => {
+    function checkUserLogin() {
+      if (
+        localStorage.getItem("userInfo") &&
+        localStorage.getItem("userInfo") !== "null"
+      ) {
+        navigate("/");
+      } else {
+        setIsLoggedInAlready(true);
+      }
+    }
+    checkUserLogin();
+  }, []);
+
   return (
     <>
-      <div className="formContainer">
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="formGroup">
-            <h1 className="formHeading">Login 🔐</h1>
-          </div>
-          <div className="formGroup">
-            <input
-              type="email"
-              placeholder="Email"
-              value={userEmail}
-              onChange={(e) => setUserEmail(e.target.value)}
-            />
-          </div>
-          <div className="formGroup">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              value={userPassword}
-              onChange={(e) => setUserPassword(e.target.value)}
-            />
-            <div onClick={handleShowhide} className="passwordShowHide">
-              {showPassword ? (
-                <FontAwesomeIcon icon={faEyeSlash} />
-              ) : (
-                <FontAwesomeIcon icon={faEye} />
-              )}
+      {isLoggedInAlready && (
+        <div className="formContainer">
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="formGroup">
+              <h1 className="formHeading">Login 🔐</h1>
             </div>
-          </div>
-          <div className="formGroup">
-            <Link to={"/forgetPass"}>Forgot Password?</Link>
-            <button type="submit"> Login </button>
-          </div>
-          <div className="formGroup">
-            <div className="lineSeperator"></div>
-          </div>
-          <div className="formGroup">
-            <p>Not Have Account?</p>
-            <Link to={"/register"} className="registerLink">
-              Register
-            </Link>
-          </div>
-          {/* <Link to={"/register"}>
+            <div className="formGroup">
+              <input
+                type="email"
+                placeholder="Email"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+              />
+            </div>
+            <div className="formGroup">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                value={userPassword}
+                onChange={(e) => setUserPassword(e.target.value)}
+              />
+              <div onClick={handleShowhide} className="passwordShowHide">
+                {showPassword ? (
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                ) : (
+                  <FontAwesomeIcon icon={faEye} />
+                )}
+              </div>
+            </div>
+            <div className="formGroup">
+              <Link to={"/forgetPass"}>Forgot Password?</Link>
+              <button type="submit"> Login </button>
+            </div>
+            <div className="formGroup">
+              <div className="lineSeperator"></div>
+            </div>
+            <div className="formGroup">
+              <p>Not Have Account?</p>
+              <Link to={"/register"} className="registerLink">
+                Register
+              </Link>
+            </div>
+            {/* <Link to={"/register"}>
             <button>Register</button>
           </Link> */}
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
